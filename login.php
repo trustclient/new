d5b2f61f4a0379a6bad35b1bdf4a4ffaafc021b4
trustclient <144108378+trustclient@users.noreply.github.com>
2025-03-17 20:58:28 +0200

    login.php

--- login.php
+++ login.php
@@ -1,10 +1,10 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-header("Content-Type: application/json");
-header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin: https://trustclient.github.io");
 header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
+
 
 $json_url = "https://raw.githubusercontent.com/Artem-Russiun/gayporno/refs/heads/main/accounts.json";
 
